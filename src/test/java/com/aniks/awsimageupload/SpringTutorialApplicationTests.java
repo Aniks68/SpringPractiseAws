eb9a7f9b4934d77d3d95d9800f66a01c139a6941
Aniks68 <ikechukwu.anene@decagon.dev>
2021-12-18 22:50:50 +0100

    first commit for initialisation of project

---
+++ src/test/java/com/aniks/awsimageupload/SpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniks.awsimageupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
